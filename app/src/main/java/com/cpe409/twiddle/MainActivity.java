35d9a34a28a0733b2a8566ad83de553f4f4312e0
= <michael.wong110@gmail.com>
2015-02-05 10:21:57 -0800

    Add parse lib

--- app/src/main/java/com/cpe409/twiddle/MainActivity.java
+++ app/src/main/java/com/cpe409/twiddle/MainActivity.java
@@ -11,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
 
+import com.parse.Parse;
+
 
 public class MainActivity extends ActionBarActivity {
 
@@ -23,6 +25,7 @@ public class MainActivity extends ActionBarActivity {
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
+
     }
 
 
