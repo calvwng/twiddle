8743bc87d288005bdd54f08326e593cd9c3e5fc9
mwong56 <michael.wong110@gmail.com>
2015-02-05 17:53:33 -0800

    Facebook login done

--- app/src/main/java/com/cpe409/twiddle/activities/LoginActivity.java
+++ app/src/main/java/com/cpe409/twiddle/activities/LoginActivity.java
@@ -1,20 +1,46 @@
 package com.cpe409.twiddle.activities;
 
 import com.cpe409.twiddle.R;
+
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.cpe409.twiddle.adapters.LoginPagerAdapter;
+import com.facebook.Request;
+import com.facebook.Response;
+import com.facebook.Session;
+import com.facebook.model.GraphUser;
+import com.facebook.widget.LoginButton;
+import com.parse.LogInCallback;
+import com.parse.ParseException;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseInstallation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
+import com.viewpagerindicator.CirclePageIndicator;
+
+import java.util.Arrays;
 
 public class LoginActivity extends ActionBarActivity {
 
+  private ProgressDialog pDialog_;
   private ViewPager viewPager_;
+  private LoginButton loginButton_;
+  private static final String TAG = LoginActivity.TAG;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +51,16 @@ public class LoginActivity extends ActionBarActivity {
     }
 
     setContentView(R.layout.activity_login);
-    if (savedInstanceState == null) {
-      getSupportFragmentManager().beginTransaction()
-          .add(R.id.container, new PlaceholderFragment())
-          .commit();
-    }
+    initializeReferences();
+
+    CirclePageIndicator login_indicator = (CirclePageIndicator) findViewById(R.id.login_indicator);
+    viewPager_.setPageTransformer(false, new FadePageTransfomer());
+
+    PagerAdapter adapter = new LoginPagerAdapter(LoginActivity.this);
+    viewPager_.setAdapter(adapter);
+
+    login_indicator.setSnap(true);
+    login_indicator.setViewPager(viewPager_);
   }
 
   private boolean isExistingUser() {
@@ -48,22 +79,90 @@ public class LoginActivity extends ActionBarActivity {
     ParseFacebookUtils.finishAuthentication(requestCode, resultCode, data);
   }
 
+  private void initializeReferences() {
+    viewPager_ = (ViewPager) findViewById(R.id.login_viewpager);
+    loginButton_ = (LoginButton) findViewById(R.id.login);
+    loginButton_.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        pDialog_ = ProgressDialog.show(LoginActivity.this, "", "Logging in...", true);
+        ParseFacebookUtils.logIn(
+            Arrays.asList(ParseFacebookUtils.Permissions.User.EMAIL, ParseFacebookUtils.Permissions.User.ABOUT_ME, "user_friends"),
+            LoginActivity.this, new LogInCallback() {
+              @Override
+              public void done(ParseUser user, ParseException err) {
+                pDialog_.dismiss();
+                if (user == null) {
+                  Log.d("Exception", err.toString());
+                  Log.d("Facebook Login", "Uh oh. The user cancelled the Facebook login.");
+//                } else if (getSharedPreferences("BOOT_PREF", MODE_PRIVATE).getBoolean("firstboot",
+//                    true)) {
+//                  getSharedPreferences("BOOT_PREF", MODE_PRIVATE).edit()
+//                      .putBoolean("firstboot", false).commit();
+//                  updateParseUserInfoInBackground();
+//                  startTutorialActivity();
+                } else {
+                  Log.d("Facebook Login", "User logged in through Facebook!");
+                  updateParseUserInfoInBackground();
+                  startMainActivity();
+                }
+              }
+            });
+      }
+    });
+  }
 
+  private void updateParseUserInfoInBackground() {
+    Request.newMeRequest(ParseFacebookUtils.getSession(), new Request.GraphUserCallback() {
+      @Override
+      public void onCompleted(GraphUser user, Response response) {
+        if (user != null) {
+          ParseUser.getCurrentUser().put("fbId", user.getId());
+          ParseUser.getCurrentUser().put("fbName", user.getName());
+          ParseUser.getCurrentUser().saveInBackground(new SaveCallback() {
 
+            @Override
+            public void done(ParseException arg0) {
+              if (arg0 != null) {
+                Log.e(TAG, "Error saving fbID and fbName in parse " + arg0.toString());
+              }
+            }
+          });
+          ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+          installation.put("fbId", user.getId());
+          installation.saveInBackground(new SaveCallback() {
 
-  /**
-   * A placeholder fragment containing a simple view.
-   */
-  public static class PlaceholderFragment extends Fragment {
+            @Override
+            public void done(ParseException arg0) {
+              if (arg0 != null) {
+                Log.e(TAG, "Error saving fbID in installation table. " + arg0.toString());
+              }
+            }
+          });
+        }
+      }
+    }).executeAsync();
+  }
 
-    public PlaceholderFragment() {
+  public void callFacebookLogout(Context context) {
+    Session session = Session.getActiveSession();
+    if (session != null) {
+
+      if (!session.isClosed()) {
+        session.closeAndClearTokenInformation();
+        // clear your preferences if saved
+      }
+    } else {
+      session = new Session(context);
+      Session.setActiveSession(session);
+      session.closeAndClearTokenInformation();
+      // clear your preferences if saved
     }
+  }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-      View rootView = inflater.inflate(R.layout.fragment_login, container, false);
-      return rootView;
+  public class FadePageTransfomer implements ViewPager.PageTransformer {
+    public void transformPage(View view, float position) {
+      view.setAlpha(1 - Math.abs(position));
     }
   }
 }
--- app/src/main/java/com/cpe409/twiddle/activities/MainActivity.java
+++ app/src/main/java/com/cpe409/twiddle/activities/MainActivity.java
@@ -10,58 +10,64 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.cpe409.twiddle.R;
+import com.facebook.AppEventsLogger;
 
 
 public class MainActivity extends ActionBarActivity {
 
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    AppEventsLogger.activateApp(this);
+    if (savedInstanceState == null) {
+      getSupportFragmentManager().beginTransaction()
+          .add(R.id.container, new PlaceholderFragment())
+          .commit();
+    }
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    AppEventsLogger.deactivateApp(this);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+  /**
+   * A placeholder fragment containing a simple view.
+   */
+  public static class PlaceholderFragment extends Fragment {
+
+    public PlaceholderFragment() {
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+      return rootView;
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+  }
 }
