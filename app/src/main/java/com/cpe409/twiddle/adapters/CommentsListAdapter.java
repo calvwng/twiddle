3fc4b3f81d0c652c2c7abc49fa6261ecaaee699a
calvwng <calvwng@gmail.com>
2015-03-16 13:31:07 -0700

    Initial commit for displaying comments (currently needs fixing)
    So I think I have everything in place for displaying (not adding) dummy
    comments in the Parse DB, but for some reason I keep getting a
    StackOverflow after my CommentListAdapter.getView returns a view...but
    the ViewHolder's data is set correctly and everything! If anybody has
    time could they take a look? This can be triggered by tapping on the
    Adventure I added called "Book Conversation Game"

---
+++ app/src/main/java/com/cpe409/twiddle/adapters/CommentsListAdapter.java
@@ -0,0 +1,72 @@
+package com.cpe409.twiddle.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cpe409.twiddle.R;
+import com.cpe409.twiddle.model.Comment;
+
+import java.util.List;
+
+/**
+ * Created by Calvin on 3/16/2015.
+ */
+public class CommentsListAdapter extends BaseAdapter {
+  private LayoutInflater mInflater;
+  private List<Comment> mComments;
+
+  public CommentsListAdapter(Context context, List<Comment> comments) {
+    mInflater = LayoutInflater.from(context);
+    mComments = comments;
+  }
+
+  @Override
+  public int getCount() {
+    return mComments.size();
+  }
+
+  @Override
+  public Object getItem(int position) {
+    return mComments.get(position);
+  }
+
+  @Override
+  public long getItemId(int position) {
+    return position;
+  }
+
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    if (position > 0) {return null}
+    View view = convertView;
+    final ViewHolder holder;
+    if(convertView == null) {
+      view = mInflater.inflate(R.layout.comment_row_layout, parent, false);
+      holder = new ViewHolder();
+      holder.profileIcon = (ImageView)view.findViewById(R.id.profile_icon);
+      holder.name = (TextView)view.findViewById(R.id.comment_author);
+      holder.text = (TextView)view.findViewById(R.id.comment_text);
+      view.setTag(holder);
+    }
+    else {
+      holder = (ViewHolder)view.getTag();
+    }
+
+    Comment comment = mComments.get(position);
+//    holder.profileIcon.setImageBitmap(comment.getProfileIconBitmap()); // Use Picasso instead
+    holder.name.setText(comment.getAuthor().getName());
+    holder.text.setText(comment.getText());
+
+    return view;
+  }
+
+  private class ViewHolder {
+    public ImageView profileIcon;
+    public TextView name, text;
+  }
+}
---
+++ app/src/main/java/com/cpe409/twiddle/model/Comment.java
@@ -0,0 +1,42 @@
+package com.cpe409.twiddle.model;
+
+import com.parse.ParseObject;
+
+/**
+ * Created by Calvin on 3/16/2015.
+ */
+public class Comment {
+  private FacebookUser author;
+  private String text;
+  private String objId;
+
+  public Comment() {
+
+  }
+
+  public Comment(String objId, FacebookUser author, String text) {
+    this.objId = objId;
+    this.author = author;
+    this.text = text;
+  }
+
+  public static Comment ParseToFeed(ParseObject parseComment, FacebookUser author) {
+    Comment comment = new Comment();
+    comment.author = author;
+    comment.text = parseComment.getString("text");
+    return comment;
+  }
+
+  public FacebookUser getAuthor() {
+    return author;
+  }
+
+  public String getText() {
+    return getText();
+  }
+
+  public String getObjId() {
+    return objId;
+  }
+
+}
